2ef3d958f27fe46814e749e89f2df981b7e98372
Patrick Grimard <pgrimard@gmail.com>
2015-07-31 08:19:16 -0400

    Added .babelrc to get property intializers to work with babel.
    Added gulp build and package.json.

---
+++ Gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var rev = require('gulp-rev');
+var inject = require('gulp-inject');
+var minifyCss = require('gulp-minify-css');
+
+var browserify = require('browserify');
+var babelify = require('babelify');
+var del = require('del');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var resolve = require('resolve');
+var series = require('stream-series');
+var argv = require('yargs').argv;
+
+var production = (process.env.NODE_ENV === 'production' || argv.production);
+
+gulp.task('default', ['scripts', 'styles'], function() {});
+
+gulp.task('styles', function() {
+  return gulp.src('./css/switch.css')
+    .pipe(minifyCss())
+    .pipe(rename('switch.min.css'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('scripts', function() {
+  var b = browserify({debug: !production})
+    .require('./src/switch.js', {entry: true})
+    .transform(babelify);
+
+  getNPMPackageIds().forEach(function(id) {
+    b.external(id);
+  });
+
+  return bundle('switch.js', b);
+});
+
+function bundle(name, b) {
+  return b.bundle().pipe(source(name))
+    .pipe(streamify(uglify()))
+    .pipe(rename(name.substring(0, name.lastIndexOf('.js')) + '.min.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/scripts'));
+}
+
+function getNPMPackageIds() {
+  // read package.json and get dependencies' package ids
+  var packageManifest = {};
+  try {
+    packageManifest = require('./package.json');
+  } catch (e) {
+    // does not have a package.json manifest
+  }
+  return Object.keys(packageManifest.dependencies) || [];
+}
