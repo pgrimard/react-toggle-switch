2da0009828080bacbf138d79bca8ba1b7fd9e32f
Patrick Grimard <pgrimard@gmail.com>
2016-04-27 20:47:51 -0400

    Updated dist.

--- dist/scripts/switch.js
+++ dist/scripts/switch.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -24,7 +24,6 @@ var Switch = (function (_React$Component) {
   _createClass(Switch, null, [{
     key: 'propTypes',
     value: {
-      value: _react2['default'].PropTypes.string,
       on: _react2['default'].PropTypes.bool,
       onClick: _react2['default'].PropTypes.func.isRequired
     },
@@ -32,7 +31,6 @@ var Switch = (function (_React$Component) {
   }, {
     key: 'defaultProps',
     value: {
-      value: '',
       on: false
     },
     enumerable: true
@@ -47,10 +45,15 @@ var Switch = (function (_React$Component) {
   }
 
   _createClass(Switch, [{
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(nextProps) {
+      this.setState({ on: nextProps.on });
+    }
+  }, {
     key: 'handleClick',
     value: function handleClick(e) {
       e.preventDefault();
-      this.props.onClick(this.props.value);
+      this.props.onClick();
       this.setState({ on: !this.state.on });
     }
   }, {
@@ -59,7 +62,7 @@ var Switch = (function (_React$Component) {
       return _react2['default'].createElement(
         'div',
         { className: 'switch ' + (this.state.on ? 'on' : ''), onClick: this.handleClick },
-        _react2['default'].createElement('div', { className: 'switch-toggle', children: this.props.children })
+        _react2['default'].createElement('div', { className: "switch-toggle", children: this.props.children })
       );
     }
   }]);
