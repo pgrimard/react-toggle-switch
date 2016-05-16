725c47f3fe5045acbfc3d7952e8b09aa7978a0fc
Patrick Grimard <pgrimard@gmail.com>
2016-05-16 14:34:16 -0400

    Updated example usage to use 2.1.0 and demonstrate enabled property.

--- example/app/scripts/main.js
+++ example/app/scripts/main.js
@@ -3,6 +3,62 @@ import ReactDom from 'react-dom';
 import Switch from 'react-toggle-switch';
 import 'styles/main.scss';
 
+class Usage1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {on: false};
+  }
+
+  render() {
+    return (
+      <div className="example-usage">
+        <h2>Basic usage</h2>
+        <p>Switch state: {this.state.on ? 'On' : 'Off'}</p>
+        <Switch onClick={() => this.setState({on: !this.state.on})} on={this.state.on}/>
+      </div>
+    );
+  }
+}
+
+class Usage2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {on: false};
+  }
+
+  render() {
+    return (
+      <div className="example-usage">
+        <h2>Toggle directly or indirectly</h2>
+        <p>Switch state: {this.state.on ? 'On' : 'Off'}</p>
+        <Switch onClick={() => this.setState({on: !this.state.on})} on={this.state.on}/>
+        <br/><br/>
+        <p>This button demonstrates how to toggle the switch indirectly by simply updating the "on" prop.</p>
+        <button onClick={() => this.setState({on: !this.state.on})}>{this.state.on ? 'Switch Off' : 'Switch On'}</button>
+      </div>
+    );
+  }
+}
+
+class Usage3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {on: false};
+  }
+
+  render() {
+    return (
+      <div className="example-usage">
+        <h2>Disabled Switch</h2>
+        <p>Off and disabled</p>
+        <Switch enabled={false}/><br/><br/>
+        <p>On and disabled</p>
+        <Switch enabled={false} on={true}/>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,14 +71,9 @@ class App extends React.Component {
     return (
       <div>
         <h1>React Toggle Switch</h1>
-        <span>Switch state: {this.state.on ? 'On' : 'Off'}</span>
-        <div>
-          <Switch onClick={() => this.setState({on: !this.state.on})} on={this.state.on}/>
-        </div>
-        <div style={{clear: 'both', marginTop: '4em'}}>
-          This button demonstrates how to toggle the switch indirectly by simply updating the "on" prop.<br/>
-          <button onClick={() => this.setState({on: !this.state.on})}>{this.state.on ? 'Switch Off' : 'Switch On'}</button>
-        </div>
+        <Usage1/>
+        <Usage2/>
+        <Usage3/>
       </div>
     );
   }
