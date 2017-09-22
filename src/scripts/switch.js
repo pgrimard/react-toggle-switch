132e2cf228b204c1b05fc764989d2164e36ce275
Patrick Grimard <pgrimard@gmail.com>
2017-09-22 15:04:44 -0400

    Fix incorrect PropTypes "required" to be "isRequired"

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 
 const propTypes = {
-  on: PropTypes.bool.required,
-  onClick: PropTypes.func.required,
+  on: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
   enabled: PropTypes.bool,
   className: PropTypes.string
 };
